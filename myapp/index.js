58cfa9ce7bc616bade1a0e87b9a34c0d3a6e87c2
jkky-98 <aal2525@ajou.ac.kr>
2024-01-26 17:39:05 +0900

    day1

---
+++ myapp/index.js
@@ -0,0 +1,21 @@
+const arr = [1, 2, 3];
+
+arr.forEach(console.log);
+
+// 6.3 template literals
+
+var template = `Template literal
+ in ES6`;
+
+console.log(template); // Template literal in ES6
+// can do multi-line strings
+
+// 6.4 string interpolation
+var first = "Jane";
+var last = "Doe";
+
+// ES6 : string interpolation
+console.log("my name is " + first + " " + last + "."); // my name is Jane Doe.
+
+console.log(`1 + 2 = ${1 + 2}`); // 1 + 2 = 3
+console.log("1 + 2 = ${1 + 2}"); // 1 + 2 = ${1 + 2}
